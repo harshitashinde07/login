5fb03d161ac068118ce71cea6076e15fab51bf17
harshitashinde07 <56025707+harshitashinde07@users.noreply.github.com>
2020-08-07 16:59:41 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,86 @@
+/*const form=document.getElementById('form');
+const user=document.getElementById('user');
+const mail=document.getElementById('mail');
+const pass=document.getElementById('pass');
+const pass1=document.getElementById('pass1');
+
+
+
+
+fuction checkInput()
+{
+  const userValue=user.value.trim();
+  const mailValue=mail.value.trim();
+  const passValue=pass.value.trim();
+  const pass1Value=pass1.value.trim();
+  if(userValue=='')
+  {
+      setErrorfor(user,username cannot be blank);
+  }
+
+else{
+    setSuccessfor(user);
+}
+
+}
+function setError(input)
+{
+  const formcontrol=input.parentElement;
+  formControl.classname=form-control error;
+}
+function setSuccess(input)
+{
+  const formcontrol=input.parentElement;
+  formControl.classname=form-control success;
+}*/
+/*function validate()
+{
+  var username=document.getElementById('user');
+  //var email=document.getElementById('mail');
+  //var password=document.getElementById('pass');
+  //var password1=document.getElementById('pass1');
+
+  if(username.value.trim())
+  {
+    document.getElementById(user).innerHTML=**please Filldata;*/
+  }
+
+/*if(username.value.trim()=='' || username.value.trim())
+{
+  alert("blank");
+}
+else
+{
+
+}*/
+
+}
+/*const username=document.getElementById('user');
+const email=document.getElementById('mail');
+const password=document.getElementById('pass');
+const passworda=document.getElementById('pass1');
+
+
+function validate()
+{
+ // const unameValue=username.value.trim();
+  const emailValue=email.value.trim();
+  const passwordValue=password.value.trim();
+  const passwordaValue=passworda.value.trim();
+  
+      if(username.value.trim()== '' )
+      {
+        //setErrorfor(username);
+        alert("blank");
+      }
+      else
+      {
+        setSuccessfor(username);
+      }
+function setErrorfor(input)
+{
+  const formControl=input.parentElement;
+  formControl.classname='form-control error';
+}
+
+}*/
